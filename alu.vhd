2235c8698fe7f65bf4c53705f7205a43549bd218
Thomas Huber <thomashuber@wifi-roaming-128-4-82-108.host.udel.edu>
2018-05-18 21:56:26 -0400

    Alu and Comparator Made

--- alu.vhd
+++ alu.vhd
@@ -6,8 +6,8 @@ entity alu is
     port(
         enable	 : in  std_logic;
         ALUOp 	 : in  std_logic_vector (2 downto 0);
-        input_A     	 : in  std_logic_vector (7 downto 0);
-        input_B     	 : in  std_logic_vector (7 downto 0);
+        A     	 : in  std_logic_vector (7 downto 0);
+        B     	 : in  std_logic_vector (7 downto 0);
         result	 : out std_logic_vector (7 downto 0) := (others => 'X');
         print 	 : out std_logic_vector (7 downto 0) := (others => 'X');
         oflow 	 : out std_logic := '0';
@@ -20,22 +20,21 @@ end alu;
 architecture structural of alu is
 	
 	component add2
-		generic (LENGTH : integer);
 		port(
-			c_op  	 : in  std_logic;
-			i_A   	 : in  std_logic_vector (LENGTH-1 downto 0);
-			i_B   	 : in  std_logic_vector (LENGTH-1 downto 0);
-			o_Z   	 : out std_logic_vector (LENGTH-1 downto 0) := (others => 'X');
-			o_flow	 : out std_logic
+			sub  	 : in  std_logic;
+			A   	 : in  std_logic_vector (LENGTH-1 downto 0);
+			B   	 : in  std_logic_vector (LENGTH-1 downto 0);
+			Sum   	 : out std_logic_vector (LENGTH-1 downto 0) := (others => 'X');
+			overflow : out std_logic
 		);
 	end component;
-	component comparator
+	component comparator2
 		generic (LENGTH : integer);
 		port(
 			input_A	 : in  std_logic_vector (LENGTH-1 downto 0);
 			input_B	 : in  std_logic_vector (LENGTH-1 downto 0);
-			output_A	 : out std_logic_vector (LENGTH-1 downto 0);
-			output_Z	 : out std_logic_vector (1 downto 0)
+			output_A : out std_logic_vector (LENGTH-1 downto 0);
+			output_Z : out std_logic_vector (1 downto 0)
 		);
 	end component;
 	
@@ -43,50 +42,50 @@ architecture structural of alu is
 	type STATE_TYPE is (load, add, sub, print, beq, noop);
 	
 	-- add/subtract signals
-	signal op : std_logic := '0';
-	signal addsub_result : std_logic_vector (7 downto 0) := (others => 'X');
-	signal flow : std_logic := 'X';
-	signal comp : std_logic_vector (1 downto 0) := "XX";
+	signal s_op : std_logic := '0';
+	signal s_addsub_result : std_logic_vector (7 downto 0) := (others => 'X');
+	signal s_flow : std_logic := 'X';
+	signal s_comp : std_logic_vector (1 downto 0) := "XX";
 	
 begin
 	
-	addsub: add2
+	addsub: add
 		generic map (LENGTH => 8)
 		port map (
-			op  	 => s_op,
-			A   	 => i_A,
-			B   	 => i_B,
-			Z   	 => s_addsub_result,
-			flow	 => s_flow
+			sub 	 => s_op,
+			A   	 => A,
+			B   	 => B,
+			Sum   	 => s_addsub_result,
+			overflow => s_flow
 		);
 	beq_comparator: comparator2
 		generic map (LENGTH => 8)
 		port map (
-			input_A	 => i_A,
-			input_B	 => i_B,
+			input_A	 => A,
+			input_B	 => B,
 			output_A => open,
 			output_Z => s_comp
 		);
 	
 	-- logic below
-	process (c_enable,
-	         c_ALUOp,
-	         i_A,
-	         i_B,
+	process (enable,
+	         ALUOp,
+	         A,
+	         B,
 	         s_addsub_result,
 	         s_comp,
 	         s_flow) is
 		variable state : STATE_TYPE;
 	begin
 		
-		if c_enable = '1' then
+		if enable = '1' then
 			
-			state_machine_case: case c_ALUOp(2 downto 1) is
+			state_machine_case: case ALUOp(2 downto 1) is
 				when "00"	=> state := load;
 				when "01"	=> state := add;
 				when "10"	=> state := sub;
 				when "11"	=>
-					special_state_case: case c_ALUOp(0) is
+					special_state_case: case ALUOp(0) is
 						when '0'	=> state := beq;
 						when '1'	=> state := print;
 						when others	=> state := noop;
@@ -97,42 +96,41 @@ begin
 			logic_case: case state is
 				when load =>
 					s_op <= '0';
-					-- ignore i_A
-					o_result <= i_B;
+					result <= B;
 					-- control signals
-					o_zero <='0';
-					o_oflow <= '0';
-					o_uflow <= '0';
+					zero <='0';
+					oflow <= '0';
+					uflow <= '0';
 				when add =>
 					s_op <= '0';
-					o_result <= s_addsub_result;
-					-- control signals
-					o_zero <= '0';
-					if s_flow = '1' then
-						if i_A(7) = '0' and i_B(7) = '0' then
-							o_oflow <= '1';
-							o_uflow <= '0';
-						elsif i_A(7) = '1' and i_B(7) = '1' then
-							o_oflow <= '0';
-							o_uflow <= '1';
-						else 
-							o_oflow <= '0';
-							o_uflow <= '0';
-						end if;
-					else
-						o_oflow <= '0';
-						o_uflow <= '0';
-					end if;
-				when sub =>
-					op <= '1';
-					result <= addsub_result;
+					result <= s_addsub_result;
 					-- control signals
 					zero <= '0';
-					if flow = '1' then
-						if i_A(7) = '0' and i_B(7) = '1' then
+					if s_flow = '1' then
+						if A(7) = '0' and B(7) = '0' then
+					n		oflow <= '1';
+							uflow <= '0';
+						elsif A(7) = '1' and B(7) = '1' then
+							oflow <= '0';
+							uflow <= '1';
+						else 
+							oflow <= '0';
+							uflow <= '0';
+						end if;
+					else
+						oflow <= '0';
+						uflow <= '0';
+					end if;
+				when sub =>
+					s_op <= '1';
+					result <= s_addsub_result;
+					------------------------- Controls -----------------------
+					zero <= '0';
+					if s_flow = '1' then
+						if A(7) = '0' and B(7) = '1' then
 							oflow <= '1';
 							uflow <= '0';
-						elsif i_A(7) = '1' and i_B(7) = '0' then
+						elsif A(7) = '1' and B(7) = '0' then
 							oflow <= '0';
 							uflow <= '1';
 						else 
@@ -144,9 +142,9 @@ begin
 						uflow <= '0';
 					end if;
 				when beq =>   -- beq (or skip)
-					op <= '1';
+					s_op <= '1';
 					result <= "XXXXXXXX";
-					if comp = "00" then
+					if s_comp = "00" then
 						zero <= '1';
 					else
 						zero <= '0';
@@ -155,18 +153,17 @@ begin
 					oflow <= '0';
 					uflow <= '0';
 				when print =>   -- print
-					op <= '0';
+					s_op <= '0';
 					result <= "XXXXXXXX";
-					print <= i_A;
-					-- ignore i_B
-					-- control signals
+					print <= A;
+					-------------------- Control --------------------
 					zero <= '0';
 					oflow <= '0';
 					uflow <= '0';
 				when noop =>   -- noop (do nothing)
-					op <= '0';
-					result <= "XXXXXXXX";
-					-- control signals
+					s_op <= '0';
+					o_result <= "XXXXXXXX";
+					----------------------- Contol ----------------------
 					zero <= '0';
 					oflow <= '0';
 					uflow <= '0';
